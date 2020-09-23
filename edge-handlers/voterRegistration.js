5c73d758d2b413ab024b8124a81a4c91bebf1ccf
Divya <divya@netlify.com>
2020-09-23 16:32:28 -0500

    fix: Trying different syntax

--- edge-handlers/voterRegistration.js
+++ edge-handlers/voterRegistration.js
@@ -1,13 +1,5 @@
-export function onRequest(event) {
-	event.respondWith(handler(event));
-}
-
-const handler = async (event) => {
-  if (event.request.path == '/secret') {
-		const resp = new Response('<h1>Access denied</h1>', {
-			status: 404,
-		});
-
-		event.respondWith(resp);
+export async function onRequest(event) {
+  if (event.request.path == '/secret' && !event.request.headers.get('Authorization') {
+    event.request.path = 'login';
   }
 }

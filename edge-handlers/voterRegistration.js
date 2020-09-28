64c7f47d88a160ef238412bf4f3d550958fdbb53
Divya <divya@netlify.com>
2020-09-28 14:28:05 -0500

    fix: update handler

--- edge-handlers/voterRegistration.js
+++ edge-handlers/voterRegistration.js
@@ -1,8 +1,4 @@
 export async function onRequest(event) {
-  await event.respondWith(handler(event));
-}
-
-const handler = async (event) => {
   const area = event.request.headers.get('X-NF-Subdivision-Code');
-  event.request.path = `${event.request.path}/state/${area.toLowerCase()}`;
+  event.request.path = `/state/${area.toLowerCase()}`;
 }

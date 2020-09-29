78de257c94d05550db9a1f4e59cbabda7ba8e6d0
Divya <divya@netlify.com>
2020-09-29 13:51:51 -0500

    fix: maybe this will work

--- edge-handlers/voterRegistration.js
+++ edge-handlers/voterRegistration.js
@@ -1,4 +1,6 @@
 export async function onRequest(event) {
   const area = event.request.headers.get('X-NF-Subdivision-Code');
-  event.request.url = `/state/${area.toLowerCase()}`;
+  // event.request.url = `/state/${area.toLowerCase()}`;
+
+  event.replaceResponse(async () => await fetch(new Request(`${event.request.url}/state/${area}`)));
 }

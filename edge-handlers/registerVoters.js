5343fc16d676d8d91bdd74ffea01f88157c1dc07
Divya <divya@netlify.com>
2020-09-30 16:42:52 -0500

    update handler

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,20 +1,9 @@
-import { HTMLRewriter } from "../utils/html-rewriter";
-
 export function onRequest(event) {
   event.replaceResponse(async () => {
-    const stateCode = event.request.headers.get('X-NF-Subdivision-Code');
-
-    let state = stateCode ? stateCode.toLowerCase() : "ca";
     const originResponse = await fetch(event.request);
-
-    const transformedBody = new HTMLRewriter()
-      .on("footer", elem => {
-        elem.replace("Made by Netlify", "text");
-      })
-      .transformInto(originResponse)
-
+    
     const headers = { 'Content-Type': 'text/html' };
 		
-    return new Response(transformedBody, { headers });
+    return new Response(originResponse, { headers });
   });
 }

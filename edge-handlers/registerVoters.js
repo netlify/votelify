36cb58da454969665893d1c38f289399c44966e3
Divya <divya@netlify.com>
2020-10-01 08:50:50 -0500

    origin response

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,3 +1,11 @@
 export function onRequest(event) {
-  console.log("nothing to see here")
+  const area = event.request.headers.get('X-NF-Subdivision-Code');
+  let state = area ? area.toLowerCase() : "il";
+  event.replaceResponse(async () => {
+    const originResponse = await fetch(new Request(`${event.request.url.replace("?#", "")}state/${state}`));
+
+    const headers = { 'Content-Type': 'text/html' };
+		
+    return new Response(originResponse, { headers });
+  });
 }

6754852e7cf3a37deb45eb2481d3ffe48c235233
Divya <divya@netlify.com>
2020-10-01 15:32:09 -0500

    brotli workaround

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,12 +1,3 @@
-export function onRequest(event) {
-  const area = event.request.headers.get('X-NF-Subdivision-Code'); //CA, NY
-  let state = area ? area.toLowerCase() : "il";
-  console.log(area)
-
-  event.replaceResponse(async () => {
-    console.log(`${event.request.url.replace("?#", "")}state/${state}`)
-    const originResponse = await fetch(event.request);
-		
-    return new Response(originResponse.body, { status: 200 });
-  });
+export function onRequest(ev) {
+  ev.replaceResponse(() => fetch(ev.request));
 }

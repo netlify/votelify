6bd5238dd3e52c6be2db9133bff86abab2db5596
Divya <divya@netlify.com>
2020-10-01 09:42:09 -0500

    handler ping

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,7 +1,9 @@
 export function onRequest(event) {
-  const area = event.request.headers.get('X-NF-Subdivision-Code');
+  const area = event.request.headers.get('X-NF-Subdivision-Code'); //CA, NY
   let state = area ? area.toLowerCase() : "il";
+
   event.replaceResponse(async () => {
+    console.log(event.request.url)
     const originResponse = await fetch(new Request(`${event.request.url.replace("?#", "")}state/${state}`));
 
     const headers = { 'Content-Type': 'text/html' };

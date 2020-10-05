bcb6dbe07f683ecd65e274eb31d9b7741d0a24a1
Divya <divya@netlify.com>
2020-10-05 17:34:14 -0500

    fix: Show default if not in us

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,12 +1,14 @@
 export function onRequest(event) {
   event.replaceResponse(async () => {
     const area = event.request.origin.subdivision;
-    const country = event.request.origin.country;
-    console.log(country);
-    let state = area && area.code ? area.code.toLowerCase() : "ca";
+    const { code } = event.request.origin.country;
 
-    const url = new URL(event.request.url);
-    url.pathname = `/state/${state}`;
+    let url = new URL(event.request.url);
+
+    if (code == "US") {
+      let state = area && area.code ? area.code.toLowerCase() : "ca";
+      url.pathname = `/state/${state}`;
+    }
 
     const originResponse = await fetch(url.toString());
     const headers = { 'Content-Type': 'text/html' };

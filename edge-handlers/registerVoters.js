2f9cfe950000165708e14b2d6c79b091df3e3948
David Calavera <david.calavera@gmail.com>
2020-10-05 16:45:46 -0700

    Fix syntax.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,12 +1,12 @@
 export function onRequest(event) {
   const { code } = event.request.origin.country;
-  
-  if (code == "US") {
-    let { code: state } = event.request.origin.subdivision;
+
+  if (code === "US") {
+    const { code: state } = event.request.origin.subdivision;
 
     let url = new URL(event.request.url);
-    url.pathname = `/state/${ state ? state.toLowerCase() : "ca" }`;
+    url.pathname = `/state/${state ? state.toLowerCase() : "ca"}`;
 
-    event.replaceResponse(async () => await fetch(url.toString()));
+    event.replaceResponse(() => fetch(url.toString()));
   }
 }

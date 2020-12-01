348783e926896626ab05dafa49a2fe100c5772d5
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2020-11-30 22:57:11 -0800

    dp test

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -5,9 +5,9 @@ export function onRequest(event) {
   if (code === "US") {
     const { code: state } = event.request.origin.subdivision;
 
-    // let url = new URL(event.request.url);
-    // url.pathname = `/state/${state ? state.toLowerCase() : "ca"}`;
+    let url = new URL(event.request.url);
+    url.pathname = `/state/${state ? state.toLowerCase() : "ca"}`;
 
-    // event.replaceResponse(() => fetch(url.toString()));
+    event.replaceResponse(() => fetch(url.toString()));
   }
 }

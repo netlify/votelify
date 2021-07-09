9e185065a59cf9b9339fcac0de42a1fcdd378d0b
RJ Beers <rj@netlify.com>
2021-07-09 17:40:49 -0400

    Update to the latest APIs and add types support

--- edge-handlers/registerVoters.js
+++ edge-handlers/registerVoters.js
@@ -1,11 +1,15 @@
-export function onRequest(event) {
-  const { code } = event.request.origin.country;
+/**
+ * @typedef {import("@netlify/edge-handler-types")}
+ * @type {EdgeHandler}
+*/
+export const onRequest = (event) => {
+  const code = event.requestMeta.origin.country?.code;
   console.log(code)
 
   if (code === "US") {
-    const { code: state } = event.request.origin.subdivision;
+    const state = event.requestMeta.origin.subdivision?.code;
 
-    let url = new URL(event.request.url);
+    let url = new URL(event.requestMeta.url);
     url.pathname = `/state/${state ? state.toLowerCase() : "ca"}`;
 
     event.replaceResponse(() => fetch(url.toString()));
